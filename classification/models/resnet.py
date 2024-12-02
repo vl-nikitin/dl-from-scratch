5bbc95e93a96fffde3c6b2f81f4f48e407909d8f
Vladimir Nikitin <wowkanwowka@yandex.ru>
2024-12-02 18:50:58 +0600

    Remove prints from resnet definition, add skip_connection_type

--- classification/models/resnet.py
+++ classification/models/resnet.py
@@ -1,24 +1,50 @@
+from classification.models.classification import ClassificationModel
 from collections import OrderedDict
-from torch.nn import Module, Conv2d, BatchNorm2d
+from torch.nn import Module, Conv2d, BatchNorm2d, Sequential, Linear, MaxPool2d, AvgPool2d, Flatten, Identity
 from torch.nn.functional import relu
+from enum import Enum
 
-class ResidualBlock(Module):
-    def __init__(self, in_channels, out_channels):
+class SkipConnectionType(Enum):
+    A = 0
+    B = 1
+    C = 2
+
+class ResidualBlockBase(Module):
+    def __init__(self):
+        super(ResidualBlockBase, self).__init__()
+
+    @staticmethod
+    def build_skip_connection(in_channels, out_channels, stride, skip_connection_type):
+        assert isinstance(skip_connection_type, SkipConnectionType)
+
+        match skip_connection_type:
+            case SkipConnectionType.A:
+                raise NotImplementedError()
+            case SkipConnectionType.B:
+                return Conv2d(in_channels, out_channels, kernel_size=1, stride=stride) if in_channels != out_channels else Identity()
+            case SkipConnectionType.C:
+                return Conv2d(in_channels, out_channels, kernel_size=1, stride=stride)
+            case _:
+                raise RuntimeError("Skip connection type must be one of 'A', 'B' or 'C'")
+
+
+class ResidualBlock(ResidualBlockBase):
+    def __init__(self, in_channels, out_channels, skip_connection_type=SkipConnectionType.B):
         super(ResidualBlock, self).__init__()
 
         needs_reduction = in_channels != out_channels
         if needs_reduction:
-            print(in_channels, out_channels)
             assert 2 * in_channels == out_channels
-        stride = 1 if not needs_reduction else 2
+
+        stride = 2 if needs_reduction else 1
         
         self.layer1 = Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
         self.bn1 = BatchNorm2d(out_channels)
         self.layer2 = Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
         self.bn2 = BatchNorm2d(out_channels)
 
-        self.sc = Conv2d(in_channels, out_channels, kernel_size=1, stride=stride)
-        
+        self.sc = self.build_skip_connection(in_channels, out_channels, stride, skip_connection_type)
+
     def forward(self, x):
         output = self.layer1(x)
         output = self.bn1(output)
@@ -29,16 +55,16 @@ class ResidualBlock(Module):
         output = self.bn2(output)
         return relu(output)
 
-class ResidualBottleneckBlock(Module):
-    def __init__(self, in_channels, out_channels):
+class ResidualBottleneckBlock(ResidualBlockBase):
+    def __init__(self, in_channels, out_channels, skip_connection_type=SkipConnectionType.B):
         super(ResidualBottleneckBlock, self).__init__()
 
-        bottleneck_channels = out_channels // 2
-        needs_reduction = in_channels != bottleneck_channels
+        bottleneck_channels = out_channels // 4
+        needs_reduction = in_channels == 2 * bottleneck_channels
         if needs_reduction:
-            print(in_channels, bottleneck_channels)
-            assert 2 * in_channels == bottleneck_channels
-        stride = 1 if not needs_reduction else 2
+            assert 2 * in_channels == out_channels
+
+        stride = 2 if needs_reduction else 1
 
         self.layer1 = Conv2d(in_channels, bottleneck_channels, kernel_size=1)
         self.bn1 = BatchNorm2d(bottleneck_channels)
@@ -47,7 +73,7 @@ class ResidualBottleneckBlock(Module):
         self.layer3 = Conv2d(bottleneck_channels, out_channels, kernel_size=1)
         self.bn3 = BatchNorm2d(out_channels)
 
-        self.sc = Conv2d(in_channels, out_channels, kernel_size=1, stride=stride)
+        self.sc = self.build_skip_connection(in_channels, out_channels, stride, skip_connection_type)
         
     def forward(self, x):
         output = self.layer1(x)
@@ -75,21 +101,20 @@ class ResNet(ClassificationModel):
     def build_convolutions(in_channels, first_layer_out_channels, blocksets_params):
         blocksets = [
             ('conv1', Conv2d(in_channels, first_layer_out_channels, kernel_size=7, stride=2, padding=3)),
-            ('pool1', torch.nn.MaxPool2d(kernel_size=2))
+            ('pool1', MaxPool2d(kernel_size=2))
         ]
         for blockset_name in blocksets_params:
-            blockset = ResNet.__build_blockset(blocksets_params[blockset_name])
+            blockset = ResNet.__build_blockset(**blocksets_params[blockset_name])
             blocksets.append((blockset_name, blockset))
-        return torch.nn.Sequential(OrderedDict(blocksets))
+        return Sequential(OrderedDict(blocksets))
 
     @staticmethod
     def build_classifier(in_params, out_params):
-        return torch.nn.Linear(in_params, out_params)
+        return Linear(in_params, out_params)
 
     @staticmethod
-    def __build_blockset(params):
+    def __build_blockset(num_blocks=2, in_channels=64, out_channels=64, is_bottleneck=False):
         blocks = []
-        num_blocks, in_channels, out_channels, is_bottleneck = params
         for i in range(num_blocks):
             block = None
             block_in_channels = in_channels if i == 0 else out_channels
@@ -99,14 +124,12 @@ class ResNet(ClassificationModel):
                 block = ResidualBottleneckBlock(in_channels=block_in_channels, out_channels=out_channels)
             blocks.append(block)
 
-        return torch.nn.Sequential(*blocks)
+        return Sequential(*blocks)
         
     def forward(self, x):
         x = self.convolutions(x)
-        print(x.shape)
         x = self.pool(x)
-        print(x.shape)
-        x = torch.nn.Flatten()(x)
+        x = Flatten()(x)
         x = self.classifier(x)
 
         return x
@@ -116,12 +139,72 @@ class ResNet18(ResNet):
         super(ResNet18, self).__init__(*args, **kwargs)
 
         self.convolutions = self.build_convolutions(in_channels, features, {
-            'conv2': [2, features, features, False],
-            'conv3': [2, features, 2 * features, False],
-            'conv4': [2, 2 * features, 4 * features, False],
-            'conv5': [2, 4 * features, 8 * features, False],
+            'conv2': {'num_blocks': 2, 'in_channels': features, 'out_channels': features, 'is_bottleneck': False},
+            'conv3': {'num_blocks': 2, 'in_channels': features, 'out_channels': 2 * features, 'is_bottleneck': False},
+            'conv4': {'num_blocks': 2, 'in_channels': 2 * features, 'out_channels': 4 * features, 'is_bottleneck': False},
+            'conv5': {'num_blocks': 2, 'in_channels': 4 * features, 'out_channels': 8 * features, 'is_bottleneck': False},
         })
 
-        self.pool = torch.nn.AvgPool2d(kernel_size=7)
+        self.pool = AvgPool2d(kernel_size=7)
 
         self.classifier = self.build_classifier(8 * features, num_classes)
+
+class ResNet34(ResNet):
+    def __init__(self, *args, in_channels=3, num_classes=10, features=64, **kwargs):
+        super(ResNet34, self).__init__(*args, **kwargs)
+
+        self.convolutions = self.build_convolutions(in_channels, features, {
+            'conv2': {'num_blocks': 3, 'in_channels': features, 'out_channels': features, 'is_bottleneck': False},
+            'conv3': {'num_blocks': 4, 'in_channels': features, 'out_channels': 2 * features, 'is_bottleneck': False},
+            'conv4': {'num_blocks': 6, 'in_channels': 2 * features, 'out_channels': 4 * features, 'is_bottleneck': False},
+            'conv5': {'num_blocks': 3, 'in_channels': 4 * features, 'out_channels': 8 * features, 'is_bottleneck': False},
+        })
+
+        self.pool = AvgPool2d(kernel_size=7)
+
+        self.classifier = self.build_classifier(8 * features, num_classes)
+
+class ResNet50(ResNet):
+    def __init__(self, *args, in_channels=3, num_classes=10, features=64, **kwargs):
+        super(ResNet50, self).__init__(*args, **kwargs)
+
+        self.convolutions = self.build_convolutions(in_channels, features, {
+            'conv2': {'num_blocks': 3, 'in_channels': features, 'out_channels': 4 * features, 'is_bottleneck': True},
+            'conv3': {'num_blocks': 4, 'in_channels': 4 * features, 'out_channels': 8 * features, 'is_bottleneck': True},
+            'conv4': {'num_blocks': 6, 'in_channels': 8 * features, 'out_channels': 16 * features, 'is_bottleneck': True},
+            'conv5': {'num_blocks': 3, 'in_channels': 16 * features, 'out_channels': 32 * features, 'is_bottleneck': True},
+        })
+
+        self.pool = AvgPool2d(kernel_size=7)
+
+        self.classifier = self.build_classifier(32 * features, num_classes)
+
+class ResNet101(ResNet):
+    def __init__(self, *args, in_channels=3, num_classes=10, features=64, **kwargs):
+        super(ResNet101, self).__init__(*args, **kwargs)
+
+        self.convolutions = self.build_convolutions(in_channels, features, {
+            'conv2': {'num_blocks': 3, 'in_channels': features, 'out_channels': 4 * features, 'is_bottleneck': True},
+            'conv3': {'num_blocks': 4, 'in_channels': 4 * features, 'out_channels': 8 * features, 'is_bottleneck': True},
+            'conv4': {'num_blocks': 23, 'in_channels': 8 * features, 'out_channels': 16 * features, 'is_bottleneck': True},
+            'conv5': {'num_blocks': 3, 'in_channels': 16 * features, 'out_channels': 32 * features, 'is_bottleneck': True},
+        })
+
+        self.pool = AvgPool2d(kernel_size=7)
+
+        self.classifier = self.build_classifier(32 * features, num_classes)
+
+class ResNet152(ResNet):
+    def __init__(self, *args, in_channels=3, num_classes=10, features=64, **kwargs):
+        super(ResNet152, self).__init__(*args, **kwargs)
+
+        self.convolutions = self.build_convolutions(in_channels, features, {
+            'conv2': {'num_blocks': 3, 'in_channels': features, 'out_channels': 4 * features, 'is_bottleneck': True},
+            'conv3': {'num_blocks': 8, 'in_channels': 4 * features, 'out_channels': 8 * features, 'is_bottleneck': True},
+            'conv4': {'num_blocks': 36, 'in_channels': 8 * features, 'out_channels': 16 * features, 'is_bottleneck': True},
+            'conv5': {'num_blocks': 3, 'in_channels': 16 * features, 'out_channels': 32 * features, 'is_bottleneck': True},
+        })
+
+        self.pool = AvgPool2d(kernel_size=7)
+
+        self.classifier = self.build_classifier(32 * features, num_classes)
--- experiments/clf.py
+++ experiments/clf.py
@@ -6,9 +6,11 @@ print(sys.path)
 
 from classification.models.classification import get_dataloaders, train
 from classification.models.alexnet import AlexNet
+from classification.models.resnet import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
 from torch.nn.functional import cross_entropy
 from torchvision.datasets import CIFAR10
 from torchvision.transforms.v2 import RandomCrop, RandomHorizontalFlip
+from torchinfo import summary
 
 def alexnet_cifar10():
     normalization = [[0.491, 0.482 ,0.446], [0.247, 0.243, 0.261]]
@@ -21,8 +23,18 @@ def alexnet_cifar10():
     trainer, callbacks = train(alexNet, train_dataloader, val_dataloader, max_epochs=2, name='AlexNet_CIFAR10', version='NewEnvTest')
     trainer.test(dataloaders=test_dataloader)
 
+def resnet():
+    # resNet = ResNet18(cross_entropy)
+    # resNet = ResNet34(cross_entropy)
+    # resNet = ResNet50(cross_entropy)
+    # resNet = ResNet101(cross_entropy)
+    resNet = ResNet152(cross_entropy)
+    print(summary(resNet, (2, 3, 224, 224)))
+
+
 def main():
-    alexnet_cifar10()
+    # alexnet_cifar10()
+    resnet()
 
 if __name__ == "__main__":
    main() 
